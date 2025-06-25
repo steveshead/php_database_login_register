995b725d428d3ca871273c7decc2b87300b17674
steveshead <steve@steve-shead.com>
2025-06-24 19:17:03 -0700

    Fixed header spacing

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
                         <h1><a href="home.php"><img class="footer-logo" src="images/favicon_dark_theme.png" width="20" height="20" alt="Logo" class="logo"> User Login & Register Script</a></h1>
                     </div>
                     <div class="column">
-                        <h3>&copy; Copyright <?=date('Y')?></h3>
+                        <h3>&copy; Copyright <?=date('Y')?> Your Name</h3>
                     </div>
                 </div>
             </div>
